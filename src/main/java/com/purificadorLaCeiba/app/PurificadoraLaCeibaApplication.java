6a5ab1c7058d0304e4619e75e31db9f49bc89018
Samayoa <Brayton-samayoa@hotmail.com>
2020-06-23 09:19:06 -0600

    Creando primer commnit

---
+++ src/main/java/com/purificadorLaCeiba/app/PurificadoraLaCeibaApplication.java
@@ -0,0 +1,13 @@
+package com.purificadorLaCeiba.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurificadoraLaCeibaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PurificadoraLaCeibaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/purificadorLaCeiba/app/PurificadoraLaCeibaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.purificadorLaCeiba.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurificadoraLaCeibaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
